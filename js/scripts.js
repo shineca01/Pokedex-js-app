4da0ac00c672ef3cddac86177595ff0e1f8926c7
shineca01 <128202204+shineca01@users.noreply.github.com>
2023-04-23 11:42:33 -0500

    update scripts.js
    update pokemonList array/scripts.js
    added 'for' loop/cripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
-let pokemonList1 = [];
+let pokemonList = [
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['fire']
+    },
+    { 
+        name: 'Venusaur',
+        height: 2,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Raichu',
+        height: 0.8,
+        types: ['electric']
+    },
+];
 
-let pokemon1 = {
-    name: 'Charmander',
-    height: 6,
-    types: ['fire']
-};
+/* Above lists the pokemon inside an array titled "pokemonList". The array contains objects with
+characteristics for each pokemon.*/
 
-let pokemon2 = {
-    name: 'Venusaur',
-    height: 8,
-    types: [grass, poison]
-};
+let pokemonLarge = ' - Wow that\'s big!';
 
-let pokemon3 = {
-    name: 'Raichu',
-    height: 5,
-    types: [electric]
-};
+for (let i=0; i < pokemonList.length; i++) {
+   if (pokemonList[i].height >= 1.7) {
+      document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')' + (pokemonLarge) + ' <br>');}
+   else {
+      document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')<br>');
+   }
+}
+
+/*this for loop has an if...else statement that identifies any pokemon greater than or equal to 1.7m.*/
