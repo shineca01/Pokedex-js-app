f8f2ced45d42cd11cf93d003b178fc3517bf3b52
shineca01 <128202204+shineca01@users.noreply.github.com>
2023-05-01 11:40:47 -0500

    updated scripts.js
    replaced for loop with forEach loop/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,14 +19,9 @@ let pokemonList = [
 /* Above lists the pokemon inside an array titled "pokemonList". The array contains objects with
 characteristics for each pokemon.*/
 
-let pokemonLarge = ' - Wow that\'s big!';
+pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + ', ' + 'height: ' + pokemon.height + '<br>') ;
+ });
 
-for (let i=0; i < pokemonList.length; i++) {
-   if (pokemonList[i].height >= 1.7) {
-      document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')' + (pokemonLarge) + ' <br>');}
-   else {
-      document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')<br>');
-   }
-}
+/*this forEach loop replaces previous for loop which had an if...else statement that identified any pokemon greater than or equal to 1.7m.*/
 
-/*this for loop has an if...else statement that identifies any pokemon greater than or equal to 1.7m.*/
