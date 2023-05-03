2ca15caf35e7efddd81b1c852f1e382610f61366
shineca01 <128202204+shineca01@users.noreply.github.com>
2023-05-03 10:09:34 -0500

    update scripts.js
    Added IIFE/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,44 @@
-let pokemonList = [
-    {
-        name: 'Charmander',
-        height: 0.6,
-        types: ['fire']
-    },
-    { 
-        name: 'Venusaur',
-        height: 2,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Raichu',
-        height: 0.8,
-        types: ['electric']
-    },
-];
+let pokemonRepository = (function () {
 
-/* Above lists the pokemon inside an array titled "pokemonList". The array contains objects with
+    /* Below lists the pokemon inside an array titled "pokemonList". The array contains objects with
 characteristics for each pokemon.*/
 
-pokemonList.forEach(function(pokemon) {
-    document.write(pokemon.name + ', ' + 'height: ' + pokemon.height + '<br>') ;
- });
+    let pokemonList = [
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ['fire']
+        },
+        { 
+            name: 'Venusaur',
+            height: 2,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Raichu',
+            height: 0.8,
+            types: ['electric']
+        },
+    ];
 
-/*this forEach loop replaces previous for loop which had an if...else statement that identified any pokemon greater than or equal to 1.7m.*/
+function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write(pokemon.name + ', ' + 'height: ' + pokemon.height + ', ' + 'types: ' + pokemon.types + '<br>') ;
+});
+
+
 
